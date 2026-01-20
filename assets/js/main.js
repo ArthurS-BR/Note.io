4a8b6e4721e14371f91edf688ab005c05187165d
ArthurS-BR <sanchesdasilvaarthur2@gmail.com>
2026-01-19 23:54:22 -0300

    Primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const btnComecar = document.getElementById("btnComecar");
+
+if (btnComecar) {
+    btnComecar.addEventListener("click", function () {
+        window.location.href = "pages/tutorial.html";
+    });
+}
